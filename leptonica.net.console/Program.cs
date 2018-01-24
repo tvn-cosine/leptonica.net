7601db125483a30ddbe9e06ef6d89b884b5d10c1
tvn-cosine <tvn-cosine>
2018-01-24 09:57:57 +0200

    added simple PixRead test

--- leptonica.net.console/Program.cs
+++ leptonica.net.console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Leptonica;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,19 @@ namespace leptonica.net.console
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string file = "Test.png";
+
+        public static void Main(string[] args)
         {
+            Pix pix = Pix.Read(file);
+            Console.WriteLine("Expected {0} and returned {1}", 1421, pix.Width);
+            Console.WriteLine("Expected {0} and returned {1}", 1949, pix.Height);
+            Console.WriteLine("Expected {0} and returned {1}", ImageFileFormatTypes.IFF_PNG, pix.InputFormat);
+            Console.WriteLine("Expected {0} and returned {1}", 96, pix.XRes);
+            Console.WriteLine("Expected {0} and returned {1}", 96, pix.YRes);
+
+            Console.WriteLine("Done...");
+            Console.ReadKey();
         }
     }
 }
---
+++ leptonica.net.test/Definitions/Pix/PixTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leptonica.Tests
+{
+    [TestClass()]
+    public class PixTests
+    {
+        private const string file = "Test.png";
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            Pix pix = Pix.Read(file);
+
+            Assert.AreEqual(1421, pix.Width);
+            Assert.AreEqual(1949, pix.Height);
+            Assert.AreEqual(ImageFileFormatTypes.IFF_PNG, pix.InputFormat);
+            Assert.AreEqual(96, pix.XRes);
+            Assert.AreEqual(96, pix.YRes);
+        }
+    }
+}
